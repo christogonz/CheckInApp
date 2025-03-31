c8e6e71e3c16db2aa9e74082f7ab016122c5dc1b
Chris Gonzalez <christogonz92@gmail.com>
2025-03-31 17:42:43 +0200

    Initial Commit

---
+++ CheckInApp/CheckInAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInAppApp.swift
+//  CheckInApp
+//
+//  Created by Christopher Gonzalez on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct CheckInAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckInApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckInApp
+//
+//  Created by Christopher Gonzalez on 2025-03-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
